a43f55a91b69a22b57116e28b863a098f6c2c904
Oleg Veschin <oveshchin@phoenixit.ru>
2024-05-24 01:19:29 +0700

    env read done
    Co-authored-by: oveshchin <oveshchin@phoenixit.ru>

--- src/envjure/envjure.clj
+++ src/envjure/envjure.clj
@@ -1,6 +1,40 @@
-(ns envjure.envjure
-  (:gen-class))
+(ns envjure
+  (:gen-class)
+  (:require [clojure.string :as str]))
 
+;; TODO: read env
+;; TODO: read yaml
+;; TODO: read json
+;; TODO: validate by schema
+;; TODO: logs
+;; TODO: generate vars
+;; TODO: generate config
+;; TODO: reload config fn
+(declare normalize read-env! read-json! read-yaml!)
+
+(defn normalize [key*]
+  (let [camel-case-regex #"(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])"]
+    (as-> key* k
+         (str/split k camel-case-regex)
+         (str/join "-" k)
+         (str/replace k #"_" "-")
+         (str/lower-case k)
+         (keyword k))))
+
+(defn coerce [schema config]
+  )
+
+(defn read-env! [env-str]
+  (->> (str/split-lines env-str)
+       (mapv (comp (fn [[key val]] (when (every? not-empty [key val]) {(normalize key) val}))
+                   #(str/split % #"\=")))
+       (into {})))
+
+(defn config []
+  (read-env!)
+  (read-json!)
+  (read-yaml!)
+  )
 
 (defn -main
   ""
--- test/envjure/envjure_test.clj
+++ test/envjure/envjure_test.clj
@@ -1,7 +1,22 @@
-(ns envjure.envjure-test
-  (:require [clojure.test :refer :all]
-            [envjure.envjure :refer :all]))
+(ns envjure-test
+  (:require [clojure.test :as t]
+            [envjure :as sut]
+            [clojure.java.io :as io]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(t/deftest normalize-test
+  (t/is (= (sut/normalize "test_string") :test-string))
+  (t/is (= (sut/normalize "test-string") :test-string))
+  (t/is (= (sut/normalize "testString") :test-string))
+  (t/is (= (sut/normalize "TestString") :test-string)))
+
+(t/deftest env-test
+  (let [env-str (slurp (io/resource ".ENV"))] ;; => "TEST=1\nTEST_2=2\nTEST-3=3\n"
+    (t/is
+     (= (sut/read-env! env-str)
+        {:test   "1"
+         :test-2 "2"
+         :test-3 "3"}))
+    (t/is (= (sut/read-env! "=1") {}))
+    (t/is (= (sut/read-env! "TEST=") {}))
+    (t/is (= (sut/read-env! "=") {}))
+    (t/is (= (sut/read-env! "") {}))))
